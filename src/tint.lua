fe943fc025d6412159dba3586c55a7d341289a5a
Optera <yousei.kyoko+github@gmail.com>
2018-11-25 11:54:50 +0100

    Delete tint.lua

--- src/tint.lua
+++
@@ -1,6 +0,0 @@
-highPressureTint = {
-  r = settings.startup["high-pressure-pipes-tint-r"].value, 
-  g = settings.startup["high-pressure-pipes-tint-g"].value, 
-  b = settings.startup["high-pressure-pipes-tint-b"].value, 
-  a = 1
-};
