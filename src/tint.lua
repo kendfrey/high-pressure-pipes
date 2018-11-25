8e9a9cb22c9daf36247c06bb40c3c5c97d4d6471
Optera <yousei.kyoko+github@gmail.com>
2018-11-25 11:54:07 +0100

    Add files via upload

---
+++ src/tint.lua
@@ -0,0 +1,6 @@
+highPressureTint = {
+  r = settings.startup["high-pressure-pipes-tint-r"].value, 
+  g = settings.startup["high-pressure-pipes-tint-g"].value, 
+  b = settings.startup["high-pressure-pipes-tint-b"].value, 
+  a = 1
+};
