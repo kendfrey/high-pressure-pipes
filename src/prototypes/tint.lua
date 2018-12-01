e7f0d8ab661259bfb00a9b9d732a3e5c0ffc39f4
Kendall Frey <kendfrey@gmail.com>
2018-11-30 19:19:50 -0500

    Tweaks to PR #1

--- src/prototypes/tint.lua
+++ src/prototypes/tint.lua
@@ -1,6 +1,7 @@
-highPressureTint = {
-  r = settings.startup["high-pressure-pipes-tint-r"].value, 
-  g = settings.startup["high-pressure-pipes-tint-g"].value, 
-  b = settings.startup["high-pressure-pipes-tint-b"].value, 
-  a = 1
+highPressureTint =
+{
+	r = settings.startup["high-pressure-pipes-tint-r"].value,
+	g = settings.startup["high-pressure-pipes-tint-g"].value,
+	b = settings.startup["high-pressure-pipes-tint-b"].value,
+	a = 1
 };
