c57ded972420fb9cc33cfce9fb3297a6a50a520a
Zevell <zevalgaming@gmail.com>
2023-12-13 22:41:06 +1000

    add 5dim "core" compatibility
    When 5dim's core mod is enabled, it alters the vanilla pipes to be in a different group and subgroup. This makes high-pressure pipes unable to be an upgrade option for the vanilla pipes. Also, it means the high-pressure pipes are not in the same group in the GUI as the vanilla pipes.
    
    This rectifies both of those issues by changing the high-pressure pipes subgroup and ordering to better align with 5dim's way of doing things, if 5dim's core mod is enabled.

--- src/data.lua
+++ src/data.lua
@@ -4,3 +4,24 @@ require("prototypes.item");
 require("prototypes.entity");
 require("prototypes.recipe");
 require("prototypes.technology");
+
+if mods["5dim_core"] then
+    local hpPipe = "high-pressure-pipe"
+    local hpPipeToGround = "high-pressure-pipe-to-ground"
+    local hpPump = "high-pressure-pump"
+    -- items
+    data.raw.item[hpPipe].subgroup = "transport-pipe"
+    data.raw.item[hpPipe].order = "ab"
+    data.raw.item[hpPipeToGround].subgroup = "transport-pipe-ground"
+    data.raw.item[hpPipeToGround].order = "ab"
+    data.raw.item[hpPump].subgroup = "liquid-small-pump"
+    data.raw.item[hpPump].order = "ab"
+
+    -- recipes
+    data.raw.recipe[hpPipe].subgroup = "transport-pipe"
+    data.raw.recipe[hpPipe].order = "ab"
+    data.raw.recipe[hpPipeToGround].subgroup = "transport-pipe-ground"
+    data.raw.recipe[hpPipeToGround].order = "ab"
+    data.raw.recipe[hpPump].subgroup = "liquid-small-pump"
+    data.raw.recipe[hpPump].order = "ab"
+end
